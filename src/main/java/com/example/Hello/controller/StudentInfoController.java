54ebbd5cef9627cfeb7334cdd6cd13cd31ad6e99
UllasCL <ullascl.1si15cs127@gmail.com>
2019-05-29 19:40:19 +0530

    get all students is working properly with exception

--- src/main/java/com/example/Hello/controller/StudentInfoController.java
+++ src/main/java/com/example/Hello/controller/StudentInfoController.java
@@ -44,12 +44,10 @@ public class StudentInfoController {
     @RequestMapping(value = "/getAllStudents", method = RequestMethod.GET)
     @ResponseBody
     public Object getAllStudentsInfo() {
-        return studentInfoService.getAllStudentsInfo().isEmpty() ? StudentRespondeDto.builder()
-                .studentInfoDtos(null)
-                .code(HttpStatus.OK.value())
-                .status(HttpStatus.OK.toString())
-                .messages(responseConstants.EMPTY_MESSAGE)
-                .build() : studentInfoService.getAllStudentsInfo();
+        if (studentInfoService.getAllStudentsInfo().isEmpty()) {
+            throw new StudentException(responseConstants.EMPTY_MESSAGE);
+        }
+        else return studentInfoService.getAllStudentsInfo();
     }
 
     /**
@@ -69,69 +67,70 @@ public class StudentInfoController {
             throw new StudentException(responseConstants.FAILED_MESSAGE);
         }
     }
-        /**
-         * add student info
-         *
-         * @param studentInfo
-         * @return
-         */
-        @RequestMapping(value = "/addStudentInfo", method = RequestMethod.POST)
-        @ResponseBody
-        public Object addStudentInfo (@RequestBody StudentInfo studentInfo){
-            if (requestValidator.validateStudentInfo(studentInfo)) {
-                return studentInfoService.addStudentInfo(studentInfo);
-            } else {
-                return StudentRespondeDto.builder()
-                        .studentInfoDtos(null)
-                        .code(HttpStatus.OK.value())
-                        .status(HttpStatus.OK.toString())
-                        .messages(responseConstants.FAILED_MESSAGE)
-                        .build();
-            }
-        }
 
-        /**
-         * delete student info
-         *
-         * @param id
-         * @return
-         */
-        @RequestMapping(value = "/deleteStudentInfo", method = RequestMethod.DELETE)
-        @ResponseBody
-        public Object deleteStudentInfo (@RequestParam Integer id){
-            if (requestValidator.validateId(id)) {
-                return
-                        StudentRespondeDto.builder()
-                                .studentInfoDtos(null)
-                                .code(HttpStatus.OK.value())
-                                .status(HttpStatus.OK.toString())
-                                .messages(responseConstants.DELETED_MESSAGE)
-                                .studentInfoDtos(studentInfoService.deleteStudentInfo(id))
-                                .build();
-            } else {
-                return StudentRespondeDto.builder()
-                        .studentInfoDtos(null)
-                        .code(HttpStatus.BAD_REQUEST.value())
-                        .status(HttpStatus.BAD_REQUEST.toString())
-                        .messages(responseConstants.FAILED_MESSAGE)
-                        .build();
-            }
-        }
-
-        /**
-         * update student info
-         *
-         * @param id
-         * @param studentInfo
-         * @return
-         */
-        @RequestMapping(value = "/updateStudentInfo", method = RequestMethod.POST)
-        @ResponseBody
-        public Object updateStudentInfo (@RequestParam Integer id, @RequestBody StudentInfo studentInfo){
-            if (requestValidator.validateId(id) && requestValidator.validateStudentInfo(studentInfo)) {
-                return studentInfoService.updateStudentInfo(id, studentInfo);
-            } else {
-                return Optional.of(responseConstants.FAILED_MESSAGE);
-            }
+    /**
+     * add student info
+     *
+     * @param studentInfo
+     * @return
+     */
+    @RequestMapping(value = "/addStudentInfo", method = RequestMethod.POST)
+    @ResponseBody
+    public Object addStudentInfo(@RequestBody StudentInfo studentInfo) {
+        if (requestValidator.validateStudentInfo(studentInfo)) {
+            return studentInfoService.addStudentInfo(studentInfo);
+        } else {
+            return StudentRespondeDto.builder()
+                    .studentInfoDtos(null)
+                    .code(HttpStatus.OK.value())
+                    .status(HttpStatus.OK.toString())
+                    .messages(responseConstants.FAILED_MESSAGE)
+                    .build();
         }
     }
+
+    /**
+     * delete student info
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/deleteStudentInfo", method = RequestMethod.DELETE)
+    @ResponseBody
+    public Object deleteStudentInfo(@RequestParam Integer id) {
+        if (requestValidator.validateId(id)) {
+            return
+                    StudentRespondeDto.builder()
+                            .studentInfoDtos(null)
+                            .code(HttpStatus.OK.value())
+                            .status(HttpStatus.OK.toString())
+                            .messages(responseConstants.DELETED_MESSAGE)
+                            .studentInfoDtos(studentInfoService.deleteStudentInfo(id))
+                            .build();
+        } else {
+            return StudentRespondeDto.builder()
+                    .studentInfoDtos(null)
+                    .code(HttpStatus.BAD_REQUEST.value())
+                    .status(HttpStatus.BAD_REQUEST.toString())
+                    .messages(responseConstants.FAILED_MESSAGE)
+                    .build();
+        }
+    }
+
+    /**
+     * update student info
+     *
+     * @param id
+     * @param studentInfo
+     * @return
+     */
+    @RequestMapping(value = "/updateStudentInfo", method = RequestMethod.POST)
+    @ResponseBody
+    public Object updateStudentInfo(@RequestParam Integer id, @RequestBody StudentInfo studentInfo) {
+        if (requestValidator.validateId(id) && requestValidator.validateStudentInfo(studentInfo)) {
+            return studentInfoService.updateStudentInfo(id, studentInfo);
+        } else {
+            return Optional.of(responseConstants.FAILED_MESSAGE);
+        }
+    }
+}
